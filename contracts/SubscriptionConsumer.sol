2e1da3ff6a29065b2a611e7f3c59732a221a5460
Jinhong Choi <65050483+gnongs@users.noreply.github.com>
2024-11-16 22:35:26 +0700

    Update SubscriptionConsumer.sol

--- contracts/SubscriptionConsumer.sol
+++ contracts/SubscriptionConsumer.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * Request testnet LINK and ETH here: https://faucets.chain.link/
@@ -41,6 +42,17 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
     bytes32 public keyHash =
         0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
 
+    // New variables
+    IERC20 public linkToken; // LINK token for deposits
+    uint256 public depositAmount; // Fixed deposit amount per user
+    enum GameState { Open, Closed, Calculating }
+    GameState public gameState;
+    address[] public players; // List of players
+    event PlayerJoined(address indexed player);
+    event WinnerDeclared(address indexed winner, uint256 reward);
+    event GameReset();
+	//
+
     // Depends on the number of requested values that you want sent to the
     // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
     // so 100,000 is a safe default for this example contract. Test and adjust
@@ -61,10 +73,46 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
      * COORDINATOR: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B
      */
     constructor(
-        uint256 subscriptionId
+        uint256 subscriptionId,
+        address _linkToken,
+        uint256 _depositAmount,
     ) VRFConsumerBaseV2Plus(0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B) {
         s_subscriptionId = subscriptionId;
+        linkToken = IERC20(_linkToken);
+        depositAmount = _depositAmount;
+        gameState = GameState.Open;
     }
+
+	// NEW FUNCTION
+    function joinGame() external {
+        require(gameState == GameState.Open, "Game is not open");
+        require(players.length < 3, "Game already has 3 players");
+        require(
+            linkToken.transferFrom(msg.sender, address(this), depositAmount),
+            "Token transfer failed"
+        );
+
+        players.push(msg.sender);
+        emit PlayerJoined(msg.sender);
+
+        if (players.length == 3) {
+            gameState = GameState.Calculating;
+			requestRandomWords(false);
+        }
+    }
+
+    function resetGame() internal {
+        delete players;
+        winner = address(0);
+        gameState = GameState.Open;
+        emit GameReset();
+    }
+
+    function getPlayers() external view returns (address[] memory) {
+        return players;
+    }
+
+	//
 
     // Assumes the subscription is funded sufficiently.
     // @param enableNativePayment: Set to `true` to enable payment in native tokens, or
@@ -106,6 +154,21 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
         emit RequestFulfilled(_requestId, _randomWords);
+		// NEW
+        require(gameState == GameState.Calculating, "Not in calculation state");
+        uint256 winnerIndex = _randomWords % players.length;
+        winner = players[winnerIndex];
+
+        uint256 totalReward = depositAmount * players.length;
+        require(
+            linkToken.transfer(winner, totalReward),
+            "Token transfer to winner failed"
+        );
+
+        emit WinnerDeclared(winner, totalReward);
+
+        resetGame();
+		//
     }
 
     function getRequestStatus(
