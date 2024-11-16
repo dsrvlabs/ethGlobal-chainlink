581e007acef43cc388b3a2b367734f605363263b
Jinhong Choi <65050483+gnongs@users.noreply.github.com>
2024-11-17 00:45:57 +0700

    Update SubscriptionConsumer.sol

--- contracts/SubscriptionConsumer.sol
+++ contracts/SubscriptionConsumer.sol
@@ -6,74 +6,44 @@ import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev
 import {VRFV2PlusClient} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
-
 contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
     event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
     struct RequestStatus {
-        bool fulfilled; // whether the request has been successfully fulfilled
-        bool exists; // whether a requestId exists
+        bool fulfilled;
+        bool exists;
         uint256[] randomWords;
     }
     mapping(uint256 => RequestStatus)
-        public s_requests; /* requestId --> requestStatus */
+        public s_requests;
 
-    // Your subscription ID.
     uint256 public s_subscriptionId;
 
-    // Past request IDs.
+
     uint256[] public requestIds;
     uint256 public lastRequestId;
 
-    // The gas lane to use, which specifies the maximum gas price to bump to.
-    // For a list of available gas lanes on each network,
-    // see https://docs.chain.link/vrf/v2-5/supported-networks
     bytes32 public keyHash =
         0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
 
-    // New variables
     IERC20 public linkToken; // LINK token for deposits
     uint256 public depositAmount; // Fixed deposit amount per user
     address public winner; // Address of the winner
 
-    enum GameState { Open, Closed, Calculating }
+    enum GameState { Open, Closed, Calculating, Calculated }
     GameState public gameState;
     address[] public players; // List of players
     event PlayerJoined(address indexed player);
     event WinnerDeclared(address indexed winner, uint256 reward);
     event GameReset();
-	//
 
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 100,000 is a safe default for this example contract. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
-    uint32 public callbackGasLimit = 1000000;
+    uint32 public callbackGasLimit = 2400000;
 
-    // The default is 3, but you can set this higher.
     uint16 public requestConfirmations = 3;
 
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2_5.MAX_NUM_WORDS.
     uint32 public numWords = 1;
 
-    /**
-     * HARDCODED FOR SEPOLIA
-     * COORDINATOR: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B
-     */
     constructor(
         uint256 subscriptionId,
         address _linkToken,
@@ -85,14 +55,9 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
         gameState = GameState.Open;
     }
 
-
-    // Assumes the subscription is funded sufficiently.
-    // @param enableNativePayment: Set to `true` to enable payment in native tokens, or
-    // `false` to pay in LINK
     function requestRandomWords(
         bool enableNativePayment
     ) internal returns (uint256 requestId) {
-        // Will revert if subscription is not set and funded.
         requestId = s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
                 keyHash: keyHash,
@@ -125,7 +90,9 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
         require(s_requests[_requestId].exists, "request not found");
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
-        emit RequestFulfilled(_requestId, _randomWords);
+        gameState = GameState.Calculated;
+
+        emit RequestFulfilled(_requestId, _randomWords);        
     }
 
     function getRequestStatus(
@@ -136,7 +103,6 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
         return (request.fulfilled, request.randomWords);
     }
 
-	// NEW FUNCTION
     function joinGame() external {
         require(gameState == GameState.Open, "Game is not open");
         require(players.length < 3, "Game already has 3 players");
@@ -166,7 +132,7 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
     }
 
     function distribute() external {
-        require(gameState == GameState.Calculating, "Not in calculation state");
+        require(gameState == GameState.Calculated, "Not in calculation state");
         uint256 winnerIndex = s_requests[lastRequestId].randomWords[0] % players.length;
         winner = players[winnerIndex];
 
@@ -180,5 +146,4 @@ contract SubscriptionConsumer is VRFConsumerBaseV2Plus {
 
         resetGame();
     }
-	//
 }
